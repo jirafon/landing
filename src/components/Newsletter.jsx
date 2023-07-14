f5ec38c4dd9bf192ee94826a03c5c83df5a3bcb2
Jirafon <chaquin@gmail.com>
2023-07-14 17:22:11 -0400

    2

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,29 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Newsletter = () => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [company, setCompany] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    const emailTemplate = {
+      name: name,
+      phone: phone,
+      company: company,
+    };
+  
+    try {
+      await axios.post('https://unbiax-main-server.onrender.com/email/send4', {
+        emails: email,
+        emailTemplate,
+      });
+      alert('Email sent successfully!');
+      setName('');
+      setPhone('');
+      setCompany('');
+      setEmail('');
+    } catch (error) {
+      console.error(error);
+      alert('Failed to send email.');
+    }
+  };
+  
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handlePhoneChange = (e) => {
+    setPhone(e.target.value);
+  };
+
+  const handleCompanyChange = (e) => {
+    setCompany(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
   return (
     <div className='w-full py-16 text-white px-4'>
       <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
         <div className='lg:col-span-2 my-4'>
           <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>
-            Quieres consejos de como optimizar tu flujo de compliance?
+            Quieres mejorar tu gestión de compliance en tu empresa?
           </h1>
           <p>Mantén tu cumplimiento con Compliax.</p>
         </div>
         <div className='my-4'>
-          <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
-            <input
-              className='p-3 flex w-full rounded-md text-black'
-              type='email'
-              placeholder='Ingrese su Email'
-            />
-            <button className='bg-[#00df9a] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3'>
-Avisarme
-            </button>
-          </div>
+          <form onSubmit={handleSubmit}>
+            <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
+              <input
+                className='p-3 flex w-full rounded-md text-black'
+                type='text'
+                placeholder='Name'
+                value={name}
+                onChange={handleNameChange}
+              />
+              <input
+                className='p-3 flex w-full rounded-md text-black'
+                type='text'
+                placeholder='Phone'
+                value={phone}
+                onChange={handlePhoneChange}
+              />
+              <input
+                className='p-3 flex w-full rounded-md text-black'
+                type='text'
+                placeholder='Company'
+                value={company}
+                onChange={handleCompanyChange}
+              />
+              <input
+                className='p-3 flex w-full rounded-md text-black'
+                type='email'
+                placeholder='Email'
+                value={email}
+                onChange={handleEmailChange}
+              />
+              <button
+                type='submit'
+                className='bg-[#00df9a] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3'
+              >
+                Contact Me
+              </button>
+            </div>
+          </form>
           <p>
-            Nos importa la gestion ordenada y segura de compliance en las empresas. Leer.{' '}
-            <span className='text-[#00df9a]'>Politica de Privacidad.</span>
+            Cómo tener una gestión ordenada y segura de compliance en las empresas.{' '}
+            <span className='text-[#00df9a]'>Política de Privacidad.</span>
           </p>
         </div>
       </div>
