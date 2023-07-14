765d68d9b5375d056beb018c53155f3b3a8a8b49
Jirafon <chaquin@gmail.com>
2023-07-13 23:34:43 -0400

    5

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,25 +9,33 @@ const Navbar = () => {
   };
 
   return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black-800'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Compliax</h1>
-      <ul className='hidden md:flex'>
+      <ul className='hidden md:flex mx-auto'>
         <li className='p-4'>Inicio</li>
-        <li className='p-4'>Empresa</li>
-        <li className='p-4'>Recursos</li>
-        <li className='p-4'>Acerca </li>
+        <li className='p-4'>Acerca</li>
         <li className='p-4'>Contacto</li>
+        <li className='p-4 menu-item-box'>
+          <button className='border-b-2 border-black-800 nav-button'>Log in</button>
+        </li>
+        <li className='p-4'>
+          <button className='border-b-2 border-black-800 nav-button'>Sign up</button>
+        </li>
       </ul>
       <div onClick={handleNav} className='block md:hidden'>
-          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+        {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
       </div>
       <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
-          <li className='p-4 border-b border-gray-600'>Inicio</li>
-          <li className='p-4 border-b border-gray-600'>Empresa</li>
-          <li className='p-4 border-b border-gray-600'>Recursos</li>
-          <li className='p-4 border-b border-gray-600'>Acerca </li>
-          <li className='p-4'>Contacto</li>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Cumpliax</h1>
+        <li className='p-4 border-b border-gray-600'>Inicio</li>
+        <li className='p-4 border-b border-gray-600'>Acerca</li>
+        <li className='p-4'>Contacto</li>
+        <li className='p-4 menu-item-box'>
+          <button className='border-b-2 border-black-800 nav-button' onClick={() => window.location.href = 'https://compliax.onrender.com'}>Entrar</button>
+        </li>
+        <li className='p-4'>
+          <button className='border-b-2 border-blue-800 nav-button'onClick={() => window.location.href = 'https://compliax.onrender.com/register'}>Registrarse </button>
+        </li>
       </ul>
     </div>
   );
