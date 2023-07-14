2a04f6514dfa40fdb0c69bd19aa4aa58f9ca8359
Jirafon <chaquin@gmail.com>
2023-07-13 22:42:28 -0400

    3

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -6,9 +6,9 @@ const Newsletter = () => {
       <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
         <div className='lg:col-span-2 my-4'>
           <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>
-            Quieres consejos de como optimizar mi flujo de compliance?
+            Quieres consejos de como optimizar tu flujo de compliance?
           </h1>
-          <p>Mantén tu negocio en cumplimiento con Compliax.</p>
+          <p>Mantén tu cumplimiento con Compliax.</p>
         </div>
         <div className='my-4'>
           <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
@@ -18,12 +18,12 @@ const Newsletter = () => {
               placeholder='Ingrese su Email'
             />
             <button className='bg-[#00df9a] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3'>
-Avisarme.
+Avisarme
             </button>
           </div>
           <p>
             Nos importa la gestion ordenada y segura de compliance en las empresas. Leer.{' '}
-            <span className='text-[#00df9a]'>Privacy Policy.</span>
+            <span className='text-[#00df9a]'>Politica de Privacidad.</span>
           </p>
         </div>
       </div>
